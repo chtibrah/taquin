cca603666d18f44dade19001f65974b304d227c8
Marouen Mhiri <marujah@gmail.com>
2017-10-10 22:56:55 +0200

    first commit

---
+++ src/components/Taquin.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="taquin">
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+    <div class="piece"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Taquin',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.taquin {
+  width: 800px;
+  height: 800px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-gap: 10px;
+  border: 1px solid #000;
+  margin: 0 auto;
+  padding: 10px;
+  .piece {
+    border: 1px solid grey;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Taquin from '@/components/Taquin'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: Taquin
+    }
+  ]
+})
