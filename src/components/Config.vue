0efd52a5496e057016e1973f63f8336474ad6e0d
Marouen Mhiri <marujah@gmail.com>
2017-10-23 21:21:35 +0200

    customize radio buttons for grid switch

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
       <nav class="game-config">
-          <div>
-            <label for="gitter-3">3x3</label><input type="radio" id="gitter-3" value="3" v-model="grid" checked="true" @change="onGridChange">
-            <label for="gitter-4">4x4</label><input type="radio" id="gitter-4" value="4" v-model="grid" @change="onGridChange">
+          <div class="grid-config">
+            <p>
+              <input type="radio" id="gitter-3" value="3" v-model="grid" checked="true" @change="onGridChange">
+              <label for="gitter-3">3x3</label>
+            </p>
+            <p>
+              <input type="radio" id="gitter-4" value="4" v-model="grid" @change="onGridChange">
+              <label bel for="gitter-4">4x4</label>
+            </p>
           </div>
           <div class="image-config">
-            <!-- <div class="numbers">
-              <input id="numbers" type="radio" @change="onPictureChange" value="none" checked="checked" v-model="picture">
-              <label for="numbers">
-                12345...
-              </label>
-            </div> -->
             <div :key="index" v-for="(pic, index) in pictureConfig">
               <input  type="radio" :id="pic.index" :value="pic.url" v-model="picture"  @change="onPictureChange" :checked="false">
               <label v-if="pic.url === 'none'" :for="pic.index" class="numbers">
@@ -56,9 +56,67 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.grid-config {
+  padding: 5px;
+  margin-bottom: 2em;
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.8);
+  [type="radio"]:checked,
+  [type="radio"]:not(:checked) {
+    position: absolute;
+    left: -9999px;
+  }
+  [type="radio"]:checked + label,
+  [type="radio"]:not(:checked) + label
+  {
+    position: relative;
+    padding-left: 28px;
+    cursor: pointer;
+    line-height: 20px;
+    display: inline-block;
+    color: #fff;
+  }
+  [type="radio"]:checked + label:before,
+  [type="radio"]:not(:checked) + label:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #ddd;
+    border-radius: 100%;
+    background: #fff;
+  }
+  [type="radio"]:checked + label:after,
+  [type="radio"]:not(:checked) + label:after {
+    content: '';
+    width: 12px;
+    height: 12px;
+    background: #3b84e8;
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+  [type="radio"]:not(:checked) + label:after {
+    opacity: 0;
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  [type="radio"]:checked + label:after {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+}
 .image-config {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+  padding: 5px;
   > div {
     display: inline-block;
     margin: 0 5px;
@@ -67,6 +125,7 @@ export default {
       justify-content: center;
       align-items: center;
       border: 1px solid #e3e3e3;
+      background-color: #fff;
     }
   }
   input[type=radio] {
