6e41e8e5d9dafb2960c3caa665fb7e5021fc1364
Marouen Mhiri <marujah@gmail.com>
2017-10-29 22:02:10 +0100

    remove unnecessary variables and add watching grid changes

--- src/components/Taquin.vue
+++ src/components/Taquin.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="main">
-    <gridconfig  @gridchanged="gridChanged"></gridconfig>
-    <pictureconfig @bgchanged="changePieceBackground"></pictureconfig>
+    <div class="config">
+      <gridconfig  @gridchanged="gridChanged"></gridconfig>
+      <pictureconfig @bgchanged="changePieceBackground"></pictureconfig>
+    </div>
     <div class="game-area">
-      <div class="backdrop">
-        <div class="modal">
-          <a class="close" href="#" @click="resetGame">×</a>
-			    <p>You win<br><br>
-            <strong>{{moves}}</strong> moves!!!
-          </p>
-        </div>
-      </div>
+      <modal :moves="moves" @reset="resetGame"></modal>
       <button class="start-game" @click="startGame">Start Game</button>
       <div class="moves">Moves: {{moves}}</div>
       <div class="taquin" :style="{
@@ -43,10 +38,11 @@
 import piece from './Piece'
 import gridconfig from './GridConfig'
 import pictureconfig from './PictureConfig'
+import modal from './Modal'
 
 export default {
   name: 'Taquin',
-  components: {piece, gridconfig, pictureconfig},
+  components: {piece, gridconfig, pictureconfig, modal},
   methods: {
     getPositions: function (factor) {
       let allPos = []
@@ -112,56 +108,33 @@ export default {
         return item
       })
       // check if game done
-      if (this.grid === 3 && newFreePosition === '22') {
-        let currentGamePieces = this.shuffledPieces.slice(0)
-        currentGamePieces = currentGamePieces.sort((a, b) => { return a.number > b.number })
-        if (this.gameIsDone(this.winningPieces3x3, currentGamePieces)) {
-          document.querySelector('.backdrop').style.opacity = '1'
-          document.querySelector('.backdrop').style.zIndex = '1'
-        }
-      }
-
-      if (this.grid === 4 && newFreePosition === '33') {
-        let currentGamePieces = this.shuffledPieces.slice(0)
-        currentGamePieces = currentGamePieces.sort((a, b) => { return a.number > b.number })
-        if (this.gameIsDone(this.winningPieces4x4, currentGamePieces)) {
-          document.querySelector('.backdrop').style.opacity = '1'
-          document.querySelector('.backdrop').style.zIndex = '1'
-        }
+      let currentGamePieces = this.shuffledPieces.slice(0)
+      currentGamePieces = currentGamePieces.sort((a, b) => { return a.number > b.number })
+      if (this.gameIsDone(this.winningPieces, currentGamePieces)) {
+        document.querySelector('.backdrop').style.opacity = '1'
+        document.querySelector('.backdrop').style.zIndex = '1'
       }
     },
     changePieceBackground: function (background) {
-      this.allPositions = this.grid === 3 ? this.allPositions3x3 : this.allPositions4x4
-      this.freePosition = this.grid === 3 ? this.freePosition3x3 : this.freePosition4x4
-      this.shuffledPieces = this.grid === 3 ? this.cloneArray(this.winningPieces3x3) : this.cloneArray(this.winningPieces4x4)
+      this.freePosition = this.allPositions[this.allPositions.length - 1]
+      this.shuffledPieces = this.cloneArray(this.winningPieces)
       this.bgStyle = background
       this.gameStarts = false
       this.randomMoves = []
-      this.firstFreePosition = this.freePosition
     },
     gridChanged: function (newGrid) {
       this.grid = newGrid
       this.pieceWidth = parseInt(578 / newGrid)
       this.pieceHeight = parseInt(578 / newGrid)
-      this.allPositions = newGrid === 3 ? this.allPositions3x3 : this.allPositions4x4
-      this.freePosition = newGrid === 3 ? this.freePosition3x3 : this.freePosition4x4
-      this.shuffledPieces = newGrid === 3 ? this.cloneArray(this.winningPieces3x3) : this.cloneArray(this.winningPieces4x4)
       this.gameStarts = false
-      this.firstFreePosition = this.freePosition
       this.randomMoves = []
     },
     resetGame: function () {
-      document.querySelector('.backdrop').style.opacity = '0'
-      document.querySelector('.backdrop').style.zIndex = '-1'
       this.gameStarts = false
+      this.moves = 0
     },
     startGame: function () {
       this.randomMoves = []
-      // this.freePosition = this.grid === 3 ? this.freePosition3x3 : this.freePosition4x4
-      // this.allPositions = this.grid === 3 ? this.allPositions3x3 : this.allPositions4x4
-      // this.freePosition = this.grid === 3 ? this.freePosition3x3 : this.freePosition4x4
-      // this.firstFreePosition = this.freePosition
-      // this.shuffledPieces = this.grid === 3 ? this.cloneArray(this.winningPieces3x3) : this.cloneArray(this.winningPieces4x4)
       let possibleMoves = this.getPossibleMoves(this.freePosition, this.allPositions)
       let count = 0
       do {
@@ -169,48 +142,36 @@ export default {
         possibleMoves = this.getPossibleMoves(this.freePosition, this.allPositions)
         count++
       } while (count < 10 || this.freePosition !== this.firstFreePosition)
-      // sort shuffled over positions
+      // sort shuffled array over positions
       this.shuffledPieces = this.shuffle(this.randomMoves).sort(function (a, b) { return (a.position > b.position) ? 1 : ((b.position > a.position) ? -1 : 0) })
       this.moves = 0
       this.gameStarts = true
     },
     cloneArray: function (inputArr) {
       return JSON.parse(JSON.stringify(inputArr))
+    },
+    calcAllPositions: function () {
+      let result = []
+      for (let i = 0; i < (this.grid * this.grid); i++) {
+        let x = Math.floor(i / this.grid)
+        let y = i % this.grid
+        result.push(x + '' + y)
+      }
+      return result
+    },
+    calcWinningPieces: function () {
+      let result = []
+      for (let i = 0; i < (this.grid * this.grid); i++) {
+        let x = Math.floor(i / this.grid)
+        let y = i % this.grid
+        result.push({'number': i + 1, 'position': x + '' + y})
+      }
+      result.pop()
+      return result
     }
   },
   data () {
     return {
-      winningPieces3x3: [
-        {number: 1, position: '00'},
-        {number: 2, position: '01'},
-        {number: 3, position: '02'},
-        {number: 4, position: '10'},
-        {number: 5, position: '11'},
-        {number: 6, position: '12'},
-        {number: 7, position: '20'},
-        {number: 8, position: '21'}
-      ],
-      winningPieces4x4: [
-        {number: 1, position: '00'},
-        {number: 2, position: '01'},
-        {number: 3, position: '02'},
-        {number: 4, position: '03'},
-        {number: 5, position: '10'},
-        {number: 6, position: '11'},
-        {number: 7, position: '12'},
-        {number: 8, position: '13'},
-        {number: 9, position: '20'},
-        {number: 10, position: '21'},
-        {number: 11, position: '22'},
-        {number: 12, position: '23'},
-        {number: 13, position: '30'},
-        {number: 14, position: '31'},
-        {number: 15, position: '32'}
-      ],
-      allPositions3x3: ['00', '01', '02', '10', '11', '12', '20', '21', '22'],
-      allPositions4x4: ['00', '01', '02', '03', '10', '11', '12', '13', '20', '21', '22', '23', '30', '31', '32', '33'],
-      freePosition3x3: '22',
-      freePosition4x4: '33',
       firstFreePosition: '',
       freePosition: '',
       shuffledPieces: [],
@@ -222,16 +183,27 @@ export default {
       pieceWidth: 0,
       pieceHeight: 0,
       gameStarts: false,
-      randomMoves: []
+      randomMoves: [],
+      winningPieces: []
     }
   },
   mounted: function () {
     this.pieceWidth = parseInt(578 / this.grid)
     this.pieceHeight = parseInt(578 / this.grid)
-    this.allPositions = this.grid === 3 ? this.allPositions3x3 : this.allPositions4x4
-    this.shuffledPieces = this.grid === 3 ? this.cloneArray(this.winningPieces3x3) : this.cloneArray(this.winningPieces4x4)
-    this.freePosition = this.grid === 3 ? this.freePosition3x3 : this.freePosition4x4
+    this.allPositions = this.calcAllPositions()
+    this.winningPieces = this.calcWinningPieces()
+    this.shuffledPieces = this.cloneArray(this.winningPieces)
+    this.freePosition = this.allPositions[this.allPositions.length - 1]
     this.firstFreePosition = this.freePosition
+  },
+  watch: {
+    grid: function () {
+      this.allPositions = this.calcAllPositions()
+      this.winningPieces = this.calcWinningPieces()
+      this.freePosition = this.allPositions[this.allPositions.length - 1]
+      this.shuffledPieces = this.cloneArray(this.winningPieces)
+      this.firstFreePosition = this.freePosition
+    }
   }
 }
 </script>
@@ -241,25 +213,24 @@ export default {
 .main {
   height: 100%;
   position: relative;
-  display: flex;
-  flex-direction: column;
-  .game-area {
-    flex: 1 1 auto;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  .config {
+    align-self: center;
   }
 }
 .start-game {
-  background: none;
   border: 1px solid #bab1ec;
   padding: 10px 10px;
   font-weight: bold;
-  background-color: #887ec1;
+  background-color: #396add;
   color: #fff;
   font-size: 1.25em;
   cursor: pointer;
   outline: 0;
   margin: 1em 0;
   &:hover {
-    background-color: #4833bf;
+    background-color: darken(#396add, 10%);
   }
 }
 .moves {
@@ -284,55 +255,6 @@ export default {
     bottom: 0;
     z-index: 1;
     cursor: not-allowed;
-  }
-}
-.backdrop {
-  position: fixed;
-  font-size: 70px;
-  color: green;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  display: flex;
-  opacity: 0;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  transition: all 0.25s ease-in-out 0s; 
-  z-index: -1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .modal {
-    position: relative;
-    width: 400px;
-    height: 180px;
-    background-color: #FFFFFF;
-    max-width: 90%;
-    max-height: 80%;
-    border-radius: 4px;
-    border: 2px solid #000000;
-    padding: 1em; 
-    overflow: hidden;
-    font-size: 16px;
-    .close{
-      position: absolute;
-      display: inline-block;
-      width: 22px;
-      height: 24px;
-      border-radius: 50%;
-      right: 10px;
-      top: 10px;
-      text-align: center;
-      transition: background-color 0.25s ease-out 0s;
-      font-size: 24px;
-      text-decoration: none;
-      line-height: 1;
-      &:hover {
-        background-color: #c3c3c3;
-      }
-    }
   }
 }
 </style>
