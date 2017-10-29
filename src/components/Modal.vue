acd0c1abb0ebf3007b79a1299ef4dbacac6f08e5
Marouen Mhiri <marujah@gmail.com>
2017-10-29 22:00:33 +0100

    add new Component Modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <a class="close" href="#" @click="resetGame">×</a>
+      <p>
+        <strong>You win</strong><br>
+        <strong>{{moves}}</strong> moves!!!
+      </p>
+      <button class="reset-btn" @click="resetGame">Ok</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'modal',
+    props: ['moves'],
+    methods: {
+      resetGame: function () {
+        document.querySelector('.backdrop').style.opacity = '0'
+        document.querySelector('.backdrop').style.zIndex = '-1'
+        this.$emit('reset')
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .backdrop {
+    position: fixed;
+    font-size: 70px;
+    color: green;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    opacity: 0;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.8);
+    transition: all 0.25s ease-in-out 0s; 
+    z-index: -1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .modal {
+      position: relative;
+      width: 400px;
+      height: 180px;
+      background-color: #FFFFFF;
+      max-width: 90%;
+      max-height: 80%;
+      border-radius: 4px;
+      border: 2px solid #000000;
+      padding: 1em; 
+      overflow: hidden;
+      font-size: 16px;
+      p {
+        line-height: 1.8;
+      }
+      .close{
+        position: absolute;
+        display: inline-block;
+        width: 22px;
+        height: 24px;
+        border-radius: 50%;
+        right: 10px;
+        top: 10px;
+        text-align: center;
+        transition: background-color 0.25s ease-out 0s;
+        font-size: 24px;
+        text-decoration: none;
+        line-height: 1;
+        &:hover {
+          background-color: #c3c3c3;
+        }
+      }
+      .reset-btn {
+        border: 1px solid green;
+        padding: 5px 10px;
+        font-weight: bold;
+        background-color: #38863a;
+        color: #fff;
+        font-size: 1.25em;
+        cursor: pointer;
+        outline: 0;
+        margin-top: 0.5em;
+        &:hover {
+          background-color: darken(#38863a, 10%);
+        }
+      }
+    }
+  }
+</style>
