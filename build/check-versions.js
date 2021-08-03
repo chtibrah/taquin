47258a4a62d20f4b946e0d26ee21e668e173b55c
Marouen Mhiri <marujah@gmail.com>
2021-08-03 20:10:30 +0200

    make it better

--- build/check-versions.js
+++ build/check-versions.js
@@ -1,27 +1,10 @@
 'use strict'
 const chalk = require('chalk')
 const semver = require('semver')
-const packageConfig = require('../package.json')
-const shell = require('shelljs')
 function exec (cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
 
-const versionRequirements = [
-  {
-    name: 'node',
-    currentVersion: semver.clean(process.version),
-    versionRequirement: packageConfig.engines.node
-  }
-]
-
-if (shell.which('npm')) {
-  versionRequirements.push({
-    name: 'npm',
-    currentVersion: exec('npm --version'),
-    versionRequirement: packageConfig.engines.npm
-  })
-}
 
 module.exports = function () {
   const warnings = []
